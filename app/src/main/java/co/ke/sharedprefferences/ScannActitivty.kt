cac826ab1bbdcfdf435399d203bbf936b0e21842
chrisribia <chrisribia@gmail.com>
2019-08-26 17:38:38 +0300

    Add scann activity for barcode reader

---
+++ app/src/main/java/co/ke/sharedprefferences/ScannActitivty.kt
@@ -0,0 +1,12 @@
+package co.ke.sharedprefferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScannActitivty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scann_actitivty)
+    }
+}
