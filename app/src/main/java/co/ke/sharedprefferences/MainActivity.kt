734971413294b7c172dacdfb03a7b60b2f0c6fb7
chrisribia <chrisribia@gmail.com>
2019-08-26 17:05:54 +0300

    Add Crude functionality

--- app/src/main/java/co/ke/sharedprefferences/MainActivity.kt
+++ app/src/main/java/co/ke/sharedprefferences/MainActivity.kt
@@ -1,12 +1,35 @@
 package co.ke.sharedprefferences
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    //saving data
+    fun saveData(view: View) {
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString("name", et_name.text.toString())
+            putInt("number", et_number.text.toString().toInt())
+            commit()
+        }
+    }
+
+    fun getData(view: View) {
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        val str_name = sharedPref.getString("name", "")
+        val int_number = sharedPref.getInt("number", 0)
+        Toast.makeText(this, "$str_name $int_number", Toast.LENGTH_LONG).show()
+
     }
 }
